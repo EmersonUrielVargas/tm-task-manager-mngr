89d646e149adaefbf8c7ea10cd8b45600b28d95a
EmersonUrielVargas <emrvargaitaz@gmail.com>
2025-03-17 12:37:31 -0500

    add health controller

---
+++ Controllers/HealthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace tm_task_manager_mngr.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new {
+                statusCode = 200,
+                status = "Healthy",
+                message = "La aplicación está funcionando correctamente" 
+            });
+        }
+    }
+    
+}
