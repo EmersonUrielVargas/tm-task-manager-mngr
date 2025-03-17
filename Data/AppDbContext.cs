6e565d7780de4a1281cd1183cc7e45229c8237e5
EmersonUrielVargas <emrvargaitaz@gmail.com>
2025-03-17 11:33:52 -0500

    first version api task-mngr

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace tm_task_manager_mngr.Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options): DbContext(options)
+    {
+        public DbSet<Task> Tasks { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+
+using Microsoft.EntityFrameworkCore;
+using tm_task_manager_mngr.Data;
+
+namespace tm_task_manager_mngr
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseInMemoryDatabase(builder.Configuration.GetConnectionString("DBTaskMemory"))
+            );
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
